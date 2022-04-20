0962fe05f4960d833e01da01b2c48e4f665c32ad
Manuel Alférez <manuelalferezruiz@gmail.com>
2022-04-20 13:40:15 +0200

    Installed Nextauth and connected with Prisma. Basic login ui

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,9 @@
 import { FC } from 'react'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 const Navbar: FC = () => {
+  const { data: session } = useSession()
+
   return (
     <div className="body-font text-gray-600">
       <div className="container mx-auto flex flex-col flex-wrap items-center p-5 md:flex-row">
@@ -23,20 +26,29 @@ const Navbar: FC = () => {
             Third Link
           </a>
         </nav>
-        <button className="mt-4 inline-flex items-center rounded border-0 bg-gray-100 py-1 px-3 text-base hover:bg-gray-200 focus:outline-none md:mt-0">
-          Button
-          <svg
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            className="ml-1 h-4 w-4"
-            viewBox="0 0 24 24"
+        {session ? (
+          <div className="flex items-center">
+            {session.user?.image && (
+              <img
+                src={session.user.image}
+                className="mr-2 mt-4 h-10 rounded-full md:ml-10 md:mt-0"
+              />
+            )}
+            <button
+              onClick={() => signOut()}
+              className="mt-4 inline-flex items-center rounded border-0 bg-gray-100 py-1 px-3 text-base hover:bg-gray-200 focus:outline-none md:mt-0"
+            >
+              Log out
+            </button>
+          </div>
+        ) : (
+          <button
+            onClick={() => signIn()}
+            className="mt-4 inline-flex items-center rounded border-0 bg-gray-100 py-1 px-3 text-base hover:bg-gray-200 focus:outline-none md:mt-0"
           >
-            <path d="M5 12h14M12 5l7 7-7 7"></path>
-          </svg>
-        </button>
+            Log in
+          </button>
+        )}
       </div>
     </div>
   )
